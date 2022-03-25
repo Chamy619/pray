60c0b1d9e68330d01ee61565f2c507f7bfe3f124
양채훈 <chamy@yangchaehun-ui-MacBookAir.local>
2022-03-25 11:01:08 +0900

    :hammer: App 클래스 이름 People로 변경하고 외부 파일로 분리

--- src/index.js
+++ src/index.js
@@ -1,48 +1,9 @@
-import Person from './Person.js';
+import People from './People.js';
 
 const $ = (element) => document.querySelector(element);
 
-class App {
-  constructor() {
-    this._people = [];
-    this._nextId = 0;
-  }
-
-  get people() {
-    return this._people;
-  }
-
-  addPerson(personName) {
-    this._people.push(new Person(personName, this._nextId++));
-  }
-
-  getPerson(id) {
-    return this._people[id];
-  }
-
-  loadPerson(person) {
-    this._people.push(person);
-  }
-
-  save() {
-    localStorage.setItem('app_info', JSON.stringify(this));
-  }
-
-  load() {
-    const appInfo = JSON.parse(localStorage.getItem('app_info'));
-    if (appInfo) {
-      appInfo._people.forEach((person) => {
-        const newPerson = new Person(person._name, person._id);
-        newPerson.prays = person._prays;
-        this.loadPerson(newPerson);
-      });
-      this._nextId = appInfo._nextId;
-    }
-  }
-}
-
 function init() {
-  const app = new App();
+  const app = new People();
   app.load();
 
   const render = () => {
